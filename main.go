46c560b79dd46b2f3f42c8ec4d1d530a9ec1efe8
Ben C <benydc@yahoo.com>
2022-05-31 16:41:33 +0300

    feat: update plugin serve witth framework

--- main.go
+++ main.go
@@ -1,12 +1,46 @@
 package main
 
 import (
-	"github.com/benydc/terraform-provider-encrypted/encrypted"
-	"github.com/hashicorp/terraform-plugin-sdk/plugin"
+	"context"
+	"flag"
+	"log"
+
+	"terraform-provider-encrypted/encrypted"
+
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 )
 
+var (
+	// these will be set by the goreleaser configuration
+	// to appropriate values for the compiled binary
+	version string = "dev"
+
+	// goreleaser can also pass the specific commit if you want
+	// commit  string = ""
+)
+
+// If you do not have terraform installed, you can remove the formatting command, but its suggested to
+// ensure the documentation is formatted properly.
+//go:generate terraform fmt -recursive ./examples/
+
+// Run the docs generation tool, check its repository for more information on how it works and how docs
+// can be customized.
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
+
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: encrypted.Provider,
-	})
+	var debug bool
+
+	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	opts := providerserver.ServeOpts{
+		Address: "registry.terraform.io/Scalingo/encrypted",
+		Debug:   debug,
+	}
+
+	err := providerserver.Serve(context.Background(), encrypted.New(version), opts)
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 }
