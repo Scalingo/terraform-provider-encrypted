64a095b48d8729b2cad2aef9d53976304f024efe
Ben C <benydc@yahoo.com>
2022-05-31 16:39:00 +0300

    test: update encrypted file specs

--- encrypted/data_ecnrypted_file_test.go
+++ encrypted/data_ecnrypted_file_test.go
@@ -1,41 +1,53 @@
 package encrypted
 
-import "reflect"
-import "testing"
+import (
+	"reflect"
+	"testing"
+
+	"github.com/zclconf/go-cty/cty"
+)
 
 func TestFlatten(t *testing.T) {
 	cases := []struct {
 		Name string
-		Map  map[string]interface{}
-		Res  map[string]interface{}
+		Map  cty.Value
+		Res  map[string]string
 	}{
 		{
 			Name: "no change to flat map",
-			Map:  map[string]interface{}{"pipo": "molo", "hello": "world"},
-			Res:  map[string]interface{}{"pipo": "molo", "hello": "world"},
+			Map: cty.ObjectVal(map[string]cty.Value{
+				"pipo":  cty.StringVal("molo"),
+				"hello": cty.StringVal("world"),
+			}),
+			Res: map[string]string{"pipo": "molo", "hello": "world"},
 		}, {
-			Name: "it should flatten sub map with '_'",
-			Map:  map[string]interface{}{"pipo": "molo", "sub": map[string]interface{}{"hello": "world"}},
-			Res:  map[string]interface{}{"pipo": "molo", "sub_hello": "world"},
+			Name: "it should flatten sub map with '.'",
+			Map: cty.ObjectVal(map[string]cty.Value{
+				"pipo": cty.StringVal("molo"),
+				"sub": cty.MapVal(map[string]cty.Value{
+					"hello": cty.StringVal("world"),
+				}),
+			}),
+			Res: map[string]string{"pipo": "molo", "sub.hello": "world"},
 		}, {
 			Name: "it should flatten sub sub map also",
-			Map: map[string]interface{}{
-				"pipo": "molo",
-				"sub": map[string]interface{}{
-					"hello": "world",
-					"foo": map[string]interface{}{
-						"bar": "value",
-					},
-				},
-			},
-			Res: map[string]interface{}{"pipo": "molo", "sub_hello": "world", "sub_foo_bar": "value"},
+			Map: cty.ObjectVal(map[string]cty.Value{
+				"pipo": cty.StringVal("molo"),
+				"sub": cty.ObjectVal(map[string]cty.Value{
+					"hello": cty.StringVal("world"),
+					"foo": cty.MapVal(map[string]cty.Value{
+						"bar": cty.StringVal("value"),
+					}),
+				}),
+			}),
+			Res: map[string]string{"pipo": "molo", "sub.hello": "world", "sub.foo.bar": "value"},
 		},
 	}
 
 	for _, c := range cases {
 		t.Run(c.Name, func(t *testing.T) {
-			if !reflect.DeepEqual(c.Res, flatten(c.Map)) {
-				t.Errorf("%v should be equal to %v", flatten(c.Map), c.Res)
+			if !reflect.DeepEqual(c.Res, FlatmapValueFromHCL2(c.Map)) {
+				t.Errorf("%v should be equal to %v", FlatmapValueFromHCL2(c.Map), c.Res)
 			}
 		})
 	}
