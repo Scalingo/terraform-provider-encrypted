d588dda81c4f2744c5a8744bc5240ee23f2ddd70
Ben C <benydc@yahoo.com>
2022-05-31 16:38:10 +0300

    feat: update provider to tfsdk

--- encrypted/provider.go
+++ encrypted/provider.go
@@ -1,28 +1,88 @@
 package encrypted
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
-func Provider() terraform.ResourceProvider {
-	return &schema.Provider{
-		Schema: map[string]*schema.Schema{
+var _ tfsdk.Provider = &encrypted{}
+
+type encrypted struct {
+	configured bool
+	version    string
+}
+
+type encryptedData struct {
+	Key types.String `tfsdk:"key"`
+}
+
+func (p *encrypted) Configure(ctx context.Context, req tfsdk.ConfigureProviderRequest, resp *tfsdk.ConfigureProviderResponse) {
+	var data encryptedData
+	diags := req.Config.Get(ctx, &data)
+	resp.Diagnostics.Append(diags...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	p.configured = true
+}
+
+func (p *encrypted) GetResources(ctx context.Context) (map[string]tfsdk.ResourceType, diag.Diagnostics) {
+	return map[string]tfsdk.ResourceType{}, nil
+}
+
+func (p *encrypted) GetDataSources(ctx context.Context) (map[string]tfsdk.DataSourceType, diag.Diagnostics) {
+	return map[string]tfsdk.DataSourceType{
+		"encrypted_file": encryptedFileDataSourceType{},
+	}, nil
+}
+
+func (p *encrypted) GetSchema(ctx context.Context) (tfsdk.Schema, diag.Diagnostics) {
+	return tfsdk.Schema{
+		Attributes: map[string]tfsdk.Attribute{
 			"key": {
-				Type:      schema.TypeString,
-				Sensitive: true,
-				Required:  true,
+				MarkdownDescription: "Master key to decrypt files",
+				Optional:            true,
+				Sensitive:           true,
+				Type:                types.StringType,
 			},
 		},
-		DataSourcesMap: map[string]*schema.Resource{
-			"encrypted_file": dataSourceEncryptedFile(),
-		},
-		ConfigureFunc: providerConfigure,
+	}, nil
+}
+
+func New(version string) func() tfsdk.Provider {
+	return func() tfsdk.Provider {
+		return &encrypted{
+			version: version,
+		}
 	}
 }
 
-func providerConfigure(data *schema.ResourceData) (interface{}, error) {
-	key := data.Get("key").(string)
+func convertProviderType(in tfsdk.Provider) (encrypted, diag.Diagnostics) {
+	var diags diag.Diagnostics
 
-	return key, nil
+	p, ok := in.(*encrypted)
+
+	if !ok {
+		diags.AddError(
+			"Unexpected Provider Instance Type",
+			fmt.Sprintf("While creating the data source or resource, an unexpected provider type (%T) was received. This is always a bug in the provider code and should be reported to the provider developers.", p),
+		)
+		return encrypted{}, diags
+	}
+
+	if p == nil {
+		diags.AddError(
+			"Unexpected Provider Instance Type",
+			"While creating the data source or resource, an unexpected empty provider instance was received. This is always a bug in the provider code and should be reported to the provider developers.",
+		)
+		return encrypted{}, diags
+	}
+
+	return *p, diags
 }
